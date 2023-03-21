e5d79694d435420a1a66260e624c369d1f7db06a
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-03-21 18:57:11 -0300

    clearing initial main template, initialized my static template base for the quiz game app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hello CLI VueJS</h1>
+    <input type="radio" name="options" value="Oculus">
+    <label>Oculus</label><br>
+
+    <input type="radio" name="options" value="Microsoft">
+    <label>Microsoft</label><br>
+
+    <input type="radio" name="options" value="Tobii">
+    <label>Tobii</label><br>
+
+    <button class="send" type="button">Confirmar</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
+//https://opentdb.com/api.php?amount=1&category=18&type=multiple
 </script>
 
 <style lang="scss">
@@ -21,6 +30,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
